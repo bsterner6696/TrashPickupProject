beea7112cf82ae51708df23d1db059e283a23205
Benjamin <bsterner6696@gmail.com>
2016-10-18 07:51:28 -0500

    Initial commit

---
+++ TrashPickupProject/TrashPickupProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrashPickupProject.Startup))]
+namespace TrashPickupProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
