f311e47f0222490a561f0564fbfabcb19c162b4e
Benjamin <bsterner6696@gmail.com>
2016-10-18 09:43:54 -0500

    made and migrated models for customer and temporary pickup

---
+++ TrashPickupProject/TrashPickupProject/Migrations/201610181431310_Added customer and temporarypickup day.cs
@@ -0,0 +1,16 @@
+namespace TrashPickupProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Addedcustomerandtemporarypickupday : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ TrashPickupProject/TrashPickupProject/Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TrashPickupProject.Models
+{
+    public class Customer
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+
+        public int Zipcode { get; set; }
+        public string StreetAddress { get; set; }
+        public string Name { get; set; }
+        public string DayOfPickup { get; set; }
+        public double Balance { get; set; }
+
+        [ForeignKey("TemporaryPickup")]
+        public int TemporaryPickupId { get; set; }
+        public TemporaryPickup TemporaryPickup { get; set; }
+    }
+}
